325c85434595f4c4754ebdb78063123446fae8c1
Alvin Kimbowa <kimbowa2alvin@gmail.com>
2022-11-01 14:38:25 +0300

    Made template general

--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 #include <cmath>
-#include "function.h"
+#include "submission.h"
 // Choose proper variable names
 // Remember to define variables with the right data types
+
 
 using namespace std;
 
 int main(){
     // Test case scenarios
-    /* r1 = 10, r2 = 10, r3 = 3: Current = 10.6667
-     * r1 = 3, r2 = 11, r3 = 32: Current = 9.10985
-    */
 
-    float r1 = 3;
-    float r2 = 11;
-    float r3 = 32;
-    cout << "Current = : " << current(r1,r2,r3) << endl;
 
     return 0;
 }
---
+++ submission.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+float current( int R1, int R2, int R3 ) {
+    // START YOUR CODE HERE
+
+    return
+    // END YOUR CODE HERE
+}
+
+#endif // FUNCTION_H
